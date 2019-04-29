d85ce823e4082409c83e5109e2614ce43dcc2550
Woopa01 <whdnwls49@naver.com>
2019-04-29 10:46:13 +0900

    [ADD] dataModel

---
+++ RxStudy-MVVM/RxStudy-MVVM/Model/historyModel.swift
@@ -0,0 +1,14 @@
+//
+//  historyModel.swift
+//  RxStudy-MVVM
+//
+//  Created by 조우진 on 25/04/2019.
+//  Copyright © 2019 조우진. All rights reserved.
+//
+
+import Foundation
+struct historyModel : Codable {
+    let historicalSiteImagePath : String
+    let historicalSiteLocation : String
+    let historicalSiteName : String
+}
